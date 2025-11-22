f2c7808139848883cfc7a2c348882ff57cae9e9d
Kannan <114098167+kannandk37@users.noreply.github.com>
2025-11-22 05:23:17 +0000

    button button

--- index.ts
+++ index.ts
@@ -1,71 +1,131 @@
 import { makeStyles } from "tss-react/mui";
-import { Theme } from "@emotion/react";
-import { IconButtonProp } from "@/design/components/button/icon/props";
+import { SimpleButtonProp } from "@/design/components/button/simple/props";
 
-export const useStyles = makeStyles<Partial<IconButtonProp>>()((theme: Theme, { disabled, variant, size, mode }) => ({
-  root: {
-    width: size ? 40 : 30,
-    height: size ? 40 : 30,
-    padding: 5,
-    backgroundColor:
-      variant == "filled" && !disabled
-        ? "var(--black-10)"
-        : variant == "filled" && disabled && "var(--grey-50) !important",
-    border:
-      variant == "outline" && !disabled && !mode
-        ? "1px solid var(--grey-20) !important"
-        : variant == "outline" && disabled && !mode
-        ? "1px solid var(--grey-40) !important"
-        : variant == "outline" && !disabled && mode
-        ? "1px solid var(--black-30) !important"
-        : variant == "outline" &&
-          disabled &&
-          mode &&
-          "1px solid var(--black-20) !important",
-    "& svg": {
-      fill:
-        variant == "filled" && !disabled
+export const useStyles = makeStyles<Partial<SimpleButtonProp>>()(
+  (theme, { width, color, variant, disabled }) => ({
+    root: {
+      minWidth: width ? `${width} !important` : "var(--buttonMinWidth) !important",
+      padding: "var(--buttonPadding)!important",
+      cursor: "var(--cursor)!important",
+      borderRadius: "var(--buttonRadius)!important",
+      border:
+        color == "secondary" && !disabled
+          ? "1px solid var(--black-20) !important" :
+          color == "secondary" && disabled ? '1px solid var(--grey-30)'
+            : "none !important",
+      textTransform: "none",
+      width: width ? width : "100%",
+      fontWeight: "var(--button-font-weight) !important",
+      fontFamily: "var(--button-font-family) !important",
+      fontSize: 'var(--button-font-size) !important',
+      lineHeight: "23px",
+      //   button background color styles
+      backgroundColor:
+        color === "primary" && !disabled
+          ? "var(--black-10) !important"
+          : color === "primary" && disabled
+            ? "var(--grey-40) !important"
+            : color === "secondary" && !disabled
+              ? "transparent !important"
+              : color === "secondary" && disabled
+                ? "var(--white-10) !important"
+                : color === "teritary"
+                  ? "rgb(170, 165, 165)!important"
+                  : color === "info"
+                    ? "var(--colorbg) !important"
+                    : variant == "text"
+                      ? "transparent !important"
+                      : width == "fullWidth"
+                        ? "var(--black-10) !important"
+                        : variant == "elevated" && !disabled ? "var(--white-10)" : variant == "elevated" && disabled ? "var(--grey-40)" : "var(--black-10) !important",
+      //   button text color styles
+      color:
+        color === "primary" && !disabled
           ? "var(--white-10) !important"
-          : variant == "filled" && disabled
-          ? "var(--grey-30) !important"
-          : disabled && !mode
-          ? "var(--grey-40) !important"
-          : mode && !disabled
-          ? "var(--white-10) !important"
-          : "var(--black-20)",
+          : color === "primary" && disabled
+            ? "var(--grey-10) !important"
+            : color === "secondary" && !disabled
+              ? "var(--black-20) !important"
+              : color === "secondary" && disabled
+                ? "var(--grey-40) !important"
+                : color === "teritary"
+                  ? "var(--color) !important"
+                  : color === "info"
+                    ? "var(--white) !important"
+                    : variant == "text" && !disabled
+                      ? "var(--infoDark) !important" :
+                      variant == "text" && disabled ? 'var(--grey-40) !important'
+                        : width == "fullWidth"
+                          ? "var(--white-10) !important"
+                          : variant == 'elevated' && !disabled ? "var(--black-20)" : variant == 'elevated' && disabled ? "var(--grey-10)" : "var(--white-10) !important",
+
+      //   hover styles
       "&:hover": {
-        fill:
-          variant == "outline" && !mode
-            ? "var(--black-20) !important"
-            : mode && !variant
-            ? "var(--brand_primary) !important"
-            : "var(--black-10)",
+        backgroundColor:
+          color === "secondary" ? "rgba(42, 42, 42, 0.08) !important" : "var(--white-10)",
+        color: variant == 'text' ? 'var(--black-10) !important' : "",
+        boxShadow:
+          color === "primary" &&
+            !disabled ?
+            "0px 4px 10px rgba(0, 0, 0, 0.4) !important" : variant === "elevated" ? "0px 2px 10px rgba(0, 0, 0, 0.25)" : "",
+        "& svg": {
+          fill: variant == 'text' && "var(--black-10)"
+        }
       },
       "&:active": {
-        fill: mode ? "var(--white-10) !important" : "var(--black-20)",
+        backgroundColor:
+          color === "primary"
+            ? "var(--black-20) !important"
+            : color === "secondary"
+              ? "rgba(42, 42, 42, 0.18) !important"
+              : color === "teritary"
+                ? "var(--white) !important"
+                : color === "info"
+                  ? "var(--colorbg) !important"
+                  : variant == "text"
+                    ? "transparent"
+                    : width == "fullWidth"
+                      ? "var(--colorbg) !important"
+                      : variant === "elevated" ? 'var(--white-20)' : '',
+        color:
+          color === "primary"
+            ? "var(--white-10) !important"
+            : color === "secondary"
+              ? "var(--black-20) !important"
+              : color === "teritary"
+                ? "var(--color) !important"
+                : color === "info"
+                  ? "var(--white) !important"
+                  : variant == "text"
+                    ? "var(--black-20) !important"
+                    : width == "fullWidth"
+                      ? "var(--white) !important"
+                      : "",
+        boxShadow:
+          color === "primary" && !disabled
+            ? "0px 1px 10px rgba(0, 0, 0, 0.25)"
+            : variant === 'elevated' ? '0px 2px 10px rgba(0, 0, 0, 0.1)' : '',
+        "& svg": {
+          fill: variant == 'text' && "var(--black-20)"
+        }
       },
-      width: size && 20,
-      height: size && 20,
+      "& svg": {
+        fill:
+          color == "primary" && !disabled
+            ? "var(--white-10)"
+            : color == "primary" && disabled
+              ? "var(--grey-10)"
+              : color == "secondary" && !disabled
+                ? "var(--black-20)"
+                : color == "secondary" && disabled
+                  ? "var(--grey-30)"
+                  : variant == "text" && !disabled ? 'var(--infoDark)' :
+                    variant == "text" && disabled ? "var(--grey-40)" : variant == "elevated" && disabled ? "var(--grey-10)" : variant == "elevated" && !disabled ? "var(--black-20) !important" : "",
+      },
+      boxShadow:
+        color === "primary" && !disabled
+          ? "0px 1px 10px rgba(0, 0, 0, 0.25)"
+          : variant === 'elevated' && !disabled ? "0px 2px 10px rgba(0, 0, 0, 0.25)" : '',
     },
-    "&:hover": {
-      backgroundColor:
-        variant == "outline" && !mode
-          ? "rgba(42, 42, 42, 0.08) !important"
-          : variant == "outline" &&
-            mode &&
-            "rgba(252, 252, 252, 0.08) !important",
-      boxShadow: variant == "filled" && "0px 4px 10px rgba(0, 0, 0, 0.4)",
-    },
-    "&:active": {
-      backgroundColor:
-        variant == "filled"
-          ? "var(--black-20) !important"
-          : variant == "outline" && !mode
-          ? "rgba(42, 42, 42, 0.18) !important"
-          : mode
-          ? "rgba(252, 252, 252, 0.18) !important"
-          : "rgba(42, 42, 42, 0.08) !important",
-      boxShadow: variant == "filled" && "0px 4px 10px rgba(0, 0, 0, 0.25)",
-    },
-  },
-}));
+  })
+);
--- index.tsx
+++ index.tsx
@@ -1,21 +1,24 @@
-import { IconButton as MUIIconButon } from '@mui/material';
-import { IconButtonProp } from '@/design/components/button/icon/props';
-import { useStyles } from '@/design/components/button/icon/style';
-// import "@/design/components/button/icon/style/index.css";
+import React from 'react';
+import { Button } from '@mui/material';
+import { SimpleButtonProp } from "@/design/components/button/simple/props";
+import { useStyles } from '@/design/components/button/simple/style';
+import "@/design/components/button/simple/style/index.css";
+import { CircularProgressBar } from '@/design/components/progress/circular';
 
-export const IconButton = ({ color, onClick, onMouseEnter, onMouseLeave, children, disabled, style, disableRipple, mode, variant, size }: IconButtonProp) => {
-    const styles = useStyles({ disabled, mode, variant, size })
+export const SimpleButton = ({ component,onClick,endIcon, children, disabled, style, startIcon, disableRipple, width, color, variant, height, iconColor, isLoading, loaderColor, loaderSize, }: SimpleButtonProp) => {
+    const styles = useStyles({ width, color, variant, disabled, height, iconColor, isLoading, loaderColor, loaderSize })
     return (
-        <MUIIconButon
+        <Button
             className={styles.classes.root}
-            onClick={onClick}
-            onMouseEnter={onMouseEnter}
-            onMouseLeave={onMouseLeave}
+            onClick={!isLoading && onClick}
             disabled={disabled}
-            disableRipple={disableRipple}
             style={style}
+            endIcon={endIcon}
+            startIcon={startIcon}
+            disableRipple={disableRipple}
+            component={component}
         >
-            {children}
-        </MUIIconButon>
+             {isLoading ? <CircularProgressBar color={loaderColor} size={loaderSize}/> : children }
+        </Button>
     )
 }
